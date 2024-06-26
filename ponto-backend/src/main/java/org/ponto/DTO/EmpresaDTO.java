69822ec4dd270e18d7f76fa5f11245b2a9b71c8e
Anna Prado <annakaroline9298@gmail.com>
2024-06-26 11:31:43 -0300

    Criado a classe EmpresaDTO.

--- ponto-backend/src/main/java/org/ponto/DTO/EmpresaDTO.java
+++ ponto-backend/src/main/java/org/ponto/DTO/EmpresaDTO.java
@@ -1,4 +1,122 @@
 package org.ponto.DTO;
 
+import java.time.LocalDate;
+
 public class EmpresaDTO {
+    private Long id;
+    private String nome;
+    private String cnpj;
+    private String endereco;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private String telefone;
+    private String email;
+    private String site;
+    private LocalDate dataFundacao;
+    private String descricao;
+
+    public EmpresaDTO() {
+    }
+
+    public EmpresaDTO(Long id, String nome, String cnpj, String endereco, String cidade, String estado, String cep,
+                      String telefone, String email, String site, LocalDate dataFundacao, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+        this.telefone = telefone;
+        this.email = email;
+        this.site = site;
+        this.dataFundacao = dataFundacao;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public LocalDate getDataFundacao() {
+        return dataFundacao;
+    }
+    public void setDataFundacao(LocalDate dataFundacao) {
+        this.dataFundacao = dataFundacao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
